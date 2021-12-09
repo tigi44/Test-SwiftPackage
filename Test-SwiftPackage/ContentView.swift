dbd659d8483a0aacfad682cc59b8f73478c97986
tigi44 <onlytg@naver.com>
2021-12-09 17:30:06 +0900

    Initial Commit

---
+++ Test-SwiftPackage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test-SwiftPackage
+//
+//  Created by tigi KIM on 2021/12/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Test-SwiftPackage/Test_SwiftPackageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_SwiftPackageApp.swift
+//  Test-SwiftPackage
+//
+//  Created by tigi KIM on 2021/12/09.
+//
+
+import SwiftUI
+
+@main
+struct Test_SwiftPackageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
